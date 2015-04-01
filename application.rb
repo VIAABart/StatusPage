929d8212699a3381583177bffa0d7109e759b8ba
Bart Debunne <bart.debunne@gmail.com>
2015-04-01 19:37:37 +0200

    Added additional timeout block and rescue.

--- application.rb
+++ application.rb
@@ -1,11 +1,12 @@
 # encoding: UTF-8
+require 'timeout'
 
 module Status
   class Application < Sinatra::Base
     
     #set :public_folder, 'public'
     
-    use Rack::Session::Pool, :expire_after => 1000 
+    #use Rack::Session::Pool, :expire_after => 1000 
     
     configure :development do
       register Sinatra::Reloader
@@ -36,17 +37,23 @@ module Status
       requests = YAML.load_file("sites.yml").map(&:symbolize_keys)
       requests.each do |r|
         begin
-          response = open(r[:uri], :allow_redirections => :safe, :read_timeout => 5).status
-          @json << {:device => r[:device], :uri => r[:uri], :status => {:code => response[1], :body => "Service is up (#{response[0]} #{response[1].capitalize})"}}
+          Timeout.timeout(1) do
+            response = open(r[:uri], :allow_redirections => :safe, :read_timeout => 5).status
+            @json << {:device => r[:device], :uri => r[:uri], :status => {:code => response[1], :body => "Service is up (#{response[0]} #{response[1].capitalize})"}}
+          end
+        rescue Timeout::Error
+          @json << {:device => r[:device], :uri => r[:uri], :status => {:code => "Fout", :body => "Connection timed out (#{$!})"}}
+          #print "Timeout::Error: #{$!}\n"
+          next
         rescue => e
           #session[:e] = e
           status = e.io.status[0]
-          puts status
           if status == '403'
             @json << {:device => r[:device], :uri => r[:uri], :status => {:code => "OK", :body => "Service is up, authenticated only (#{e})"}}
           else
             @json << {:device => r[:device], :uri => r[:uri], :status => {:code => "Fout", :body => "Service is down #{e}"}}
           end
+          next
         end
       end
       erb :stats
